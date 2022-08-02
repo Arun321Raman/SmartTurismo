12e8d7fe1ca8d865a9c17eb4ccfc8ad0bab2e577
stan5370 <32014878+stan5370@users.noreply.github.com>
2022-08-02 19:22:34 -0400

    added comments

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const express = require('express');
         const fs = require('fs');
         var info = []
 
-
+//setup for dynamic data webpage
         app.set('view engine', 'ejs');
 
         app.use(function(req, res, next) {
@@ -15,15 +15,16 @@ const express = require('express');
 
         app.use(express.static('public'));
 
-
+//send pred.js when requested
         app.get('/pred.js', function(req, res) {
             res.sendFile(__dirname + '/pred.js');
         });
 
-
+//display the data page when requested
         app.get('/data', function (req, res) {
             res.render('pages/data', {
                 // EJS variable and server-side variable
+                // displays as one big block of information, doesn't update in real time (needs a page refresh)
                 info: info
             });
         });
@@ -31,7 +32,7 @@ const express = require('express');
 
         app.use(express.text())
 
-
+// receiving the navigator.sendBeacon request
         app.post('/',function(req,res){
             console.log('i got the request',req.body)
             var reqBody = JSON.parse(req.body);
@@ -39,7 +40,7 @@ const express = require('express');
             var rawG = reqBody.a;
             var rawWC = reqBody.g;
             var rawPC = reqBody.i;
-
+//calculations for the actual predictions, based on linear regression
  var standardA = (rawA - 13.47826087)/16.62;
             var standardG = (rawG - 0.6086956522)/1.90;
  if (((508.341 * standardA) + (139.125 * standardG) - 31.186) >= 0){
@@ -58,10 +59,11 @@ var finalPred = "YES"}
                 + '-' + pad(now.getDate())
                 + ' (' + pad(now.getHours()-5)
                 + ':' + pad(now.getMinutes()) + ')';
-
+//add the formatted information to the data page
             info.push("Date (Time):   " + formattedTime + "      |    Where:   " + reqBody.url + "      |    Prediction:   " + finalPred + " - WordCount: " + rawWC + " - PictureCount: " + rawPC)
         });
 
         app.listen(port, () => {
+                // this is a lie
             console.log(`Example app listening at http://localhost:${port}`)
 })

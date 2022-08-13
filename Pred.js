ea7dcd4220d08d071001c03307bc7a499941d30b
Arun321Raman <108631761+Arun321Raman@users.noreply.github.com>
2022-08-13 19:31:27 -0400

    Update Pred.js

--- Pred.js
+++ Pred.js
@@ -1,4 +1,4 @@
-if (window.location.href.includes(("/department/") || ("/listing/") || ("/things-to-do/") || ("events/") || ("restaurants/") || ("hotels/") || ("meetings-groups") || ("sports/") || ("wedding"))) {
+if (window.location.href.includes(("/department/") || ("/listing/") || ("/things-to-do/") || ("events/") || ("restaurants/") || ("hotels/") || ("meetings-groups") || ("sports/") || ("wedding”)));
     var c = 0;
     var e = new Date().valueOf();
     var finalPred;
@@ -11,41 +11,42 @@ if (window.location.href.includes(("/department/") || ("/listing/") || ("/things
 
     function getWordCount() {
         try {
-            var body = top.document.querySelector("body");
-            if (body) {
-                var content = body.innerText || getInnerText(top.document.body, top);
-                content = content.replace(/\n/ig, ' ');
-                var wordCount = content.match(/\S+/gi).length;
-                return wordCount;
+        var body = top.document.querySelector("body");
+        if (body) {
+            var content = body.innerText || getInnerText(top.document.body, top);
+            content = content.replace(/\n/ig, ' ');
+            var wordCount = content.match(/\S+/gi).length;
+            return wordCount;
             }
-        } catch (e) {
+            } catch (e) {
             processError("getWordCount", e);
         }
     }
 
-    function getKeyWordPresence() {
-        var body = top.document.querySelector("body");
-        var KeyWordPresence = document.body.innerText.includes("Example");
-        return KeyWordPresence;
-    }
+function getKeyWordPresence(){
+var body = top.document.querySelector("body");
+var KeyWordPresence = document.body.innerText.includes("Example");
+return KeyWordPresence;}
 
-    var count = 0;
-    window.onload = function() {
-        var elems = document.body.getElementsByTagName("*");
-        for (var i = 0; i < elems.length; i++) {
-            var properties = (elems[i].currentStyle || window.getComputedStyle(elems[i], false));
-            background = properties.backgroundImage.slice(4, -1);
-            if (background.indexOf("http://placehold.it/50x50") > -1) {
-                count++;
-            }
-        }
-    };
+var count = 0;
+window.onload = function () {
+  var elems = document.body.getElementsByTagName("*");
+  for(var i = 0; i < elems.length; i++)
+  {
+      var properties = (elems[i].currentStyle || window.getComputedStyle(elems[i], false));
+      background = properties.backgroundImage.slice(4, -1);
+      if(background.indexOf("http://placehold.it/50x50") > -1)
+      {
+         count++;
+      }
+  }
+};
 
-    $(window).on("beforeunload", function() {
+ $(window).on("beforeunload", function() {
         var f = getWordCount();
         var d = (new Date().valueOf() - e) / 1000;
         var h = count;
         var currenturl = window.location.href;
-        navigator.sendBeacon('INSERT_URL_HERE', JSON.stringify({ a: c, b: d, url: currenturl, g: f, i: h }));
-    })
-}
+ navigator.sendBeacon('http://216.137.179.27:3000/', JSON.stringify({a:c, b:d, url:currenturl, g:f, i:h}));
+})
+
